c60b72e7d2524aaf2eb318e5f2750e1d9cef6846
Joseph Mansfield <sftrabbit@gmail.com>
2013-01-25 16:51:02 +0000

    Create initial Android project
    Create a bare bones Android application given by 'android create
    project'. Tidy up some of the default files by removing comments,
    sorting out formatting, etc. and make the application look prettier
    with the light variation of the Holo theme.
    
    Also provide a standard Android .gitignore file to avoid adding
    generated or local files to version control.

---
+++ src/uk/co/sftrabbit/stacksync/StackSync.java
@@ -0,0 +1,15 @@
+package uk.co.sftrabbit.stacksync;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class StackSync extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
