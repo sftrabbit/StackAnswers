60d7f3e5e10f5d238f7b2b6954c2215445acfd3f
Joseph Mansfield <sftrabbit@gmail.com>
2013-11-18 12:03:09 +0000

    Update progress bar as authentication pages load

--- src/uk/co/sftrabbit/stackanswers/AuthenticationActivity.java
+++ src/uk/co/sftrabbit/stackanswers/AuthenticationActivity.java
@@ -1,9 +1,11 @@
 package uk.co.sftrabbit.stackanswers;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 import uk.co.sftrabbit.stackanswers.app.DrawerActivity;
 
 public class AuthenticationActivity extends DrawerActivity {
@@ -15,8 +17,17 @@ public class AuthenticationActivity extends DrawerActivity {
 
 		setDrawerIndicatorEnabled(false);
 
+		final ProgressBar progressBar =
+			(ProgressBar) findViewById(R.id.authentication_progress);
+
 		WebView webView = (WebView) findViewById(R.id.authentication_web_view);
 		webView.setWebViewClient(new WebViewClient());
+		webView.setWebChromeClient(new WebChromeClient() {
+			@Override
+			public void onProgressChanged(WebView webview, int newProgress) {
+				progressBar.setProgress(newProgress);
+			}
+		});
 
 		WebSettings webSettings = webView.getSettings();
 		webSettings.setJavaScriptEnabled(true);
