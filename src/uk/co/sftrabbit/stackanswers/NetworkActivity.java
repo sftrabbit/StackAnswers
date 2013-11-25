07970291d5bfb11256a4e159368efc2006283f68
Joseph Mansfield <sftrabbit@gmail.com>
2013-11-25 00:33:14 +0000

    Add code documentation to NetworkActivity.java

--- src/uk/co/sftrabbit/stackanswers/NetworkActivity.java
+++ src/uk/co/sftrabbit/stackanswers/NetworkActivity.java
@@ -41,10 +41,64 @@ import java.util.Collections;
 import java.util.Arrays;
 import uk.co.sftrabbit.stackanswers.app.BaseActivity;
 
+/**
+ * An activity that gives the user a global view of the Stack Exchange network.
+ * <p> 
+ * <code>NetworkActivity</code> is the root activity of Stack Answers. It is the
+ * first activity the user will see and helps them navigate to the content
+ * they're interested in. It presents content from all Stack Exchange sites and
+ * tailors that content to the user's preferences and prior activity.
+ * <p>
+ * The activity is divided into two tabs:
+ * <ol>
+ * <li>The "What's hot" tab aims to provide the user with the latest popular
+ *     content from their Stack Exchange sites. It allows the user to quickly
+ *     delve into the deep and interesting areas of the application.
+ * <li>The "Sites" tab gives a list of all Stack Exchange sites ordered
+ *     according to the user's prior activity, together with the users's account
+ *     information for those sites. It provides a shallower form of navigation
+ *     so the user can choose exactly which sites to browse in detail. This is
+ *     implemented in {@link SitesFragment}.
+ * </ol>
+ * <p>
+ * When the user is not logged in, content will be presented entirely according
+ * to popularity amongst all Stack Exchange users.
+ * 
+ * @author Joseph Mansfield
+ * @see SitesFragment
+ **/
 public class NetworkActivity extends BaseActivity {
+
+	/**
+	 * Name of extra data for identifying which tab should be selected when this
+	 * activity is launched by {@link android.content.Intent}. The associated data
+	 * must be one of {@link #TAB_NONE}, {@link #TAB_HOT}, and {@link #TAB_SITES}.
+	 * 
+	 * @see android.content.Intent#putExtra(String,int)
+	 */
 	public static final String EXTRA_TAB = "NetworkActivityTab";
+
+	/**
+	 * Extra data that specifies that the currently selected tab will not change
+	 * if the activity is not being created (such as when it is already at the top
+	 * of the back stack). 
+	 * 
+	 * @see #EXTRA_TAB
+	 */
 	public static final int TAB_NONE = -1;
+
+	/**
+	 * Extra data that specifies that the "What's hot" tab should be selected.
+	 * 
+	 * @see #EXTRA_TAB
+	 */
 	public static final int TAB_HOT = 0;
+
+	/**
+	 * Extra data that specifies that the "Sites" tab should be selected.
+	 * 
+	 * @see #EXTRA_TAB
+	 */
 	public static final int TAB_SITES = 1;
 
 	private ActionBar actionBar;
@@ -57,6 +111,9 @@ public class NetworkActivity extends BaseActivity {
 			new TabSpec(R.string.page_sites, 0, SitesFragment.class)
 		));
 
+	/**
+	 * Initialises the activity layout and navigation tabs.
+	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -72,6 +129,9 @@ public class NetworkActivity extends BaseActivity {
 		selectRequestedTab();
 	}
 
+	/**
+	 * Initialises the relevent actions for this activity.
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		final MenuInflater inflater = getMenuInflater();
@@ -80,6 +140,10 @@ public class NetworkActivity extends BaseActivity {
 		return super.onCreateOptionsMenu(menu);
 	}
 
+	/**
+	 * Presents a site-specific dropdown menu to the user for extra navigation
+	 * options.
+	 */
 	public void onDropdownClick(View v) {
 		//TODO - Inflate this menu instead of hardcoding it
 		final PopupMenu popupMenu = new PopupMenu(this, v);
@@ -89,12 +153,19 @@ public class NetworkActivity extends BaseActivity {
 		popupMenu.show();
 	}
 
+	/**
+	 * Selects the tab requested by the {@link android.content.Intent}.
+	 */
 	@Override
 	protected void onNewIntent(Intent intent) {
 		setIntent(intent);
 		selectRequestedTab();
 	}
 
+	/**
+	 * Initialises the tab pager to show the appropriate fragments and listen
+	 * for tab selection changes.
+	 */
 	private void initTabPager() {
 		tabPager = (ViewPager) findViewById(R.id.tab_pager);
 		assert tabPager != null : "No tab pager in activity";
@@ -103,6 +174,9 @@ public class NetworkActivity extends BaseActivity {
 		tabPager.setOnPageChangeListener(new TabPagerListener(actionBar));
 	}
 
+	/**
+	 * Adds the appropriate tabs to the activity action bar.
+	 */
 	private void initTabs() {
 		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
@@ -111,12 +185,23 @@ public class NetworkActivity extends BaseActivity {
 		}
 	}
 
+	/**
+	 * Adds a tab to the activity action bar according to the given
+	 * <code>TabSpec</code>.
+	 * 
+	 * @param tabSpec specification of the tab to add
+	 */
 	private void addTab(TabSpec tabSpec) {
 		final ActionBar.Tab tab = actionBar.newTab();
 		tab.setTabListener(new TabListener(tabPager, getDrawerLayout()));
 		actionBar.addTab(tabSpec.applyTo(tab));
 	}
 
+	/**
+	 * Select the tab requested by the intent that launched this activity.
+	 * 
+	 * @see #EXTRA_TAB
+	 */
 	private void selectRequestedTab() {
 		Intent intent = getIntent();
 		Bundle extras = intent.getExtras();
@@ -128,11 +213,25 @@ public class NetworkActivity extends BaseActivity {
 		}
 	}
 
+	/**
+	 * A specification that describes an action bar tab. Includes resource IDs for
+	 * the tab's text label and content description, and provides a
+	 * {@link android.app.Fragment} class to instantiate for that tab.
+	 */
 	private static class TabSpec {
 		private final int tabTextResourceId;
 		private final int contentDescriptionResourceId;
 		private final Class fragmentClass;
 
+		/**
+		 * Constructs a <code>TabSpec</code> with the given properties.
+		 * 
+		 * @param tabTextResourceId string resource ID for the tab text
+		 * @param contentDescriptionResourceId string resource ID for the tab
+		 *        content description
+		 * @param fragmentClass a subclass of {@link android.app.Fragment} to
+		 *        instantiate for this tab
+		 */
 		public TabSpec(int tabTextResourceId, int contentDescriptionResourceId,
 		               Class fragmentClass) {
 			this.tabTextResourceId = tabTextResourceId;
@@ -140,6 +239,12 @@ public class NetworkActivity extends BaseActivity {
 			this.fragmentClass = fragmentClass;
 		}
 
+		/**
+		 * Applies this tab specification to the given tab.
+		 * 
+		 * @param tab the tab to apply this specification to
+		 * @return the modified tab
+		 */
 		public ActionBar.Tab applyTo(ActionBar.Tab tab) {
 			if (tabTextResourceId != 0) {
 				tab.setText(tabTextResourceId);
@@ -150,6 +255,11 @@ public class NetworkActivity extends BaseActivity {
 			return tab;
 		}
 
+		/**
+		 * Instantiates the {@link android.app.Fragment} for this tab.
+		 * 
+		 * @return the instantiated <code>Fragment</code>
+		 */
 		public Fragment createFragment() {
 			try {
 				if (fragmentClass == null) {
@@ -163,50 +273,104 @@ public class NetworkActivity extends BaseActivity {
 		}
 	}
 
+	/**
+	 * An view pager adapter that pages fragments according to the activity's tab
+	 * specifications.
+	 * 
+	 * @see android.support.v4.view.ViewPager
+	 * @see TabSpec
+	 */
 	private static class TabPagerAdapter extends FragmentPagerAdapter {
+
 		public TabPagerAdapter(FragmentManager fragmentManager) {
 			super(fragmentManager);
 		}
 
+		/**
+		 * Gets the number of fragments to present.
+		 *
+		 * @return the number of fragments
+		 */
 		@Override
 		public int getCount() {
 			return TAB_SPECS.size();
 		}
 
+		/**
+		 * Gets the fragment corresponding to <code>position</code>.
+		 * 
+		 * @param position the position
+		 * @return the fragment at <code>position</code>
+		 */
 		@Override
 		public Fragment getItem(int position) {
 			return TAB_SPECS.get(position).createFragment();
 		}
 	}
 
+	/**
+	 * A view pager listener that selects the corresponding tab when the page
+	 * changes.
+	 * 
+	 * @see android.support.v4.view.ViewPager
+	 */
 	private static class TabPagerListener extends
 			ViewPager.SimpleOnPageChangeListener {
 		private final ActionBar actionBar;
 
+		/**
+		 * Constructs a listener that will select tabs from the given action bar.
+		 * 
+		 * @param actionBar the action bar to select tabs from
+		 */
 		public TabPagerListener(ActionBar actionBar) {
 			this.actionBar = actionBar;
 		}
 
+		/**
+		 * Selects the corresponding tab for the given page position.
+		 * 
+		 * @param position the page position that has been selected
+		 */
 		@Override
 		public void onPageSelected(int position) {
 			actionBar.setSelectedNavigationItem(position);
 		}
 	}
 
+	/**
+	 * An action bar tab listener that listens for tab selections and adjusts the
+	 * view pager accordingly, closing any drawers that might get in the way.
+	 */
 	private static class TabListener implements ActionBar.TabListener {
 		private final ViewPager tabPager;
 		private final DrawerLayout drawerLayout;
 
+		/**
+		 * Constructs a <code>TabListener</code> that will select the corresponding
+		 * pages from the given {@link android.support.v4.view.ViewPager} and close
+		 * all drawers in the given {@link android.support.v4.widget.DrawerLayout}.
+		 * 
+		 * @param tabPager the pager to select corresponding pages from
+		 * @param drawerLayout the drawer layout to close drawers in
+		 */
 		public TabListener(ViewPager tabPager, DrawerLayout drawerLayout) {
 			this.tabPager = tabPager;
 			this.drawerLayout = drawerLayout;
 		}
 
+		/**
+		 * Closes all open drawers when a tab is reselected.
+		 */
 		public void onTabReselected(ActionBar.Tab tab,
 		                            FragmentTransaction transaction) {
 			drawerLayout.closeDrawers();
 		}
 
+		/**
+		 * Closes all open drawers and changes the current page when a tab is
+		 * selected.
+		 */
 		public void onTabSelected(ActionBar.Tab tab,
 		                          FragmentTransaction transaction) {
 			drawerLayout.closeDrawers();
